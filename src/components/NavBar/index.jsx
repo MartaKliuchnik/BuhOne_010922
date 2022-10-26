0f51b713352a1196b935d710b5312691f670968c
Marta <martasagun95@gmail.com>
2022-10-26 16:13:08 +0200

    added useRef on header menu

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import s from './style.module.sass';
 import logo from './media/buh_one_logo.png'
 
 export default function NavBar() {
+
+    const header_menu_ref = useRef();
+
+    const nav_btn_click = () => {
+        header_menu_ref.current.classList.toggle(s.active);
+    }
 
     return (
         <nav className={['wrapper', s.wrapper].join(' ')}>
@@ -12,17 +18,14 @@ export default function NavBar() {
                 </a>
             </div> 
             <div className={s.header_menu}>
-                <button><i class={["fa-solid fa-bars", s.open_header_menu]. join(' ')}></i></button>
-                <ul className={s.nav_menu}>
+                <button onClick={nav_btn_click}><i class={["fa-solid fa-bars", s.open_header_menu].join(' ')}></i></button>
+                <ul ref={header_menu_ref} className={s.nav_menu}>
                     <li>Главная</li>
                     <li>услуги</li>
                     <li>кейсы</li>
                     <li>о компании</li>
                     <li>контакты</li>
                 </ul>
-                <div className={s.contact_btn}>
-                    <button>Связаться</button>
-                </div>
             </div>
         </nav>
     )
